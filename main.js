63f9424db39aca5f02dc7837ad2f4a3cedf5523a
Carlos04 <cvargaslopez769@gmail.com>
2021-03-26 15:46:06 -0600

    .

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-// implementar modo oscuro
-
+//Implementar modo oscuro
+//Transiciones a la secccion de "Trusted by brands "
+//Respuestas interactivas para el usuario seccion "Questions of clients"
