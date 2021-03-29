f8051a85b6ff2f2e9687e79d081ac67046d5af5e
Carlos04 <cvargaslopez769@gmail.com>
2021-03-28 21:22:06 -0600

    carlos

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 //Implementar modo oscuro
+
+const switchButton = document.getElementById('switch')
+
+ 
+switchButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark') //toggle the HTML body the class 'dark'
+    switchButton.classList.toggle('active')//toggle the HTML button with the id='switch' with the class 'active'
+})
+
+
 //Transiciones a la secccion de "Trusted by brands "
 //Respuestas interactivas para el usuario seccion "Questions of clients"
+
